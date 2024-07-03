d1540ffa9c522bf5089179953d0494a7d69b83b3
shamito <shamito@google.com>
2024-07-03 15:43:50 -0400

    ng g all three component sections

---
+++ src/app/writing/writing.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-writing',
+  standalone: true,
+  imports: [],
+  templateUrl: './writing.component.html',
+  styleUrl: './writing.component.scss'
+})
+export class WritingComponent {
+
+}
