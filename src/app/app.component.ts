4b1c42c7ec94006e93ea3c0b18b1274524ef41d3
shamito <shamito@google.com>
2024-07-03 12:53:18 -0400

    start working on main

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,27 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { MainComponent } from './main/main.component';
+import { PortfolioService } from './services/portfolio.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, MainComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'personal-portfolio';
+
+  portfolioTitle: String = "";
+
+  constructor(private portfolioService: PortfolioService){}
+
+  ngOnInit(): void {
+    this.portfolioService.getpPortfolioName().subscribe((data)=>{
+      this.portfolioTitle = data;
+    })
+  }
 }
