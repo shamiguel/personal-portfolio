3fbd4147128fef3ff4f23502115b01840522480a
shamito <shamito@google.com>
2024-07-03 15:01:10 -0400

    fix test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -24,6 +24,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, personal-portfolio');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Shami Tomita-Rodriguez');
   });
 });
