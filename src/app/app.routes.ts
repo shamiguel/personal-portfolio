acc73f52c7a271e5903a61727b9a720803910d50
shamito <shamito@google.com>
2024-07-26 12:50:28 -0400

    set up routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { ExperienceComponent } from './experience/experience.component';
+import { WritingComponent } from './writing/writing.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { MainComponent } from './main/main.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: MainComponent},
+    {path: 'experience', component: ExperienceComponent},
+    {path: 'writings', component: WritingComponent},
+    {path: 'projects', component: ProjectsComponent},
+];
--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -5,12 +5,13 @@ import { CommonModule } from '@angular/common';
 import { FontAwesomeModule, FaIconLibrary } from '@fortawesome/angular-fontawesome';
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import { faGithub, faLinkedin, faMedium } from '@fortawesome/free-brands-svg-icons';
+import { MainSectionComponent } from '../main-section/main-section.component';
 
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [CommonModule, FontAwesomeModule],
+  imports: [CommonModule, FontAwesomeModule, MainSectionComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
@@ -23,6 +24,29 @@ export class MainComponent {
 
   portfolioTitle: string = "";
   blurb: string = "";
+
+  experiences:Content[] = [];
+  experiencesSection:Section = {
+    name: '',
+    display: false,
+    content: this.experiences
+  };
+
+  projects:Content[] = [];
+  projectsSection:Section = {
+    name: '',
+    display: false,
+    content: this.projects
+  };
+
+  writings:Content[] = [];
+  writingsSection:Section = {
+    name: '',
+    display: false,
+    content: this.writings
+  };
+
+
   constructor(private portfolioService: PortfolioService){}
 
   ngOnInit(): void {
@@ -30,6 +54,19 @@ export class MainComponent {
       this.portfolioTitle = data.portfolioName;
       this.blurb = data.blurb
     })
+    this.portfolioService.getExperiences().subscribe((data)=>{
+      this.experiencesSection = data;
+      this.experiences = this.experiencesSection.content;
+    })
+    this.portfolioService.getProjects().subscribe((data)=>{
+      this.projectsSection = data;
+      this.projects = this.projectsSection.content;
+    })
+    this.portfolioService.getWritings().subscribe((data)=>{
+      this.writingsSection = data;
+      this.writings = this.writingsSection.content;
+    })
   }
+
 
 }
